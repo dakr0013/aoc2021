ddcae7e1da26299086a4f804f8fbea50452100a5
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-03 08:06:25 +0100

    day3
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- src/main/kotlin/com/example/Day03Part1.kt
+++ src/main/kotlin/com/example/Day03Part1.kt
@@ -1,5 +1,31 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(3, 1)
+  val binaryNumbers = readFileString(3, 1)
+
+  var gammaRate = ""
+  var epsilonRate = ""
+  for (column in 0..11) {
+    var countOnes = 0
+    for (row in binaryNumbers.indices) {
+      val num = binaryNumbers[row][column]
+      if (num == '1') {
+        countOnes++
+      }
+    }
+
+        if (countOnes > 500) {
+          gammaRate +="1"
+          epsilonRate +="0"
+        } else {
+          gammaRate +="0"
+          epsilonRate +="1"
+        }
+  }
+
+  val gamma = Integer.parseInt(gammaRate,2)
+  val epsilon = Integer.parseInt(epsilonRate,2)
+  println(gamma)
+  println(epsilon)
+  println(gamma*epsilon)
 }
--- src/main/kotlin/com/example/Day03Part2.kt
+++ src/main/kotlin/com/example/Day03Part2.kt
@@ -1,5 +1,58 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(3, 2)
+  val binaryNumbers = readFileString(3, 1)
+
+  val oxygen = findOxygen(binaryNumbers, 0)
+  val co2 = findCO2(binaryNumbers, 0)
+
+  println(oxygen)
+  println(co2)
+  println(oxygen*co2)
+}
+
+fun findOxygen(numbers: List<String>, columnIndex: Int): Int {
+  if (numbers.size == 1) {
+    return Integer.parseInt(numbers.first(),2)
+  } else {
+    val ones = mutableListOf<String>()
+    val zeros = mutableListOf<String>()
+    for (row in numbers.indices) {
+      val num = numbers[row][columnIndex]
+      if (num == '1') {
+        ones.add(numbers[row])
+      } else {
+        zeros.add(numbers[row])
+      }
+    }
+
+    return if (ones.size >= zeros.size) {
+      findOxygen(ones, columnIndex + 1)
+    } else {
+      findOxygen(zeros, columnIndex + 1)
+    }
+  }
+}
+
+fun findCO2(numbers: List<String>, columnIndex: Int): Int {
+  if (numbers.size == 1) {
+    return Integer.parseInt(numbers.first(),2)
+  } else {
+    val ones = mutableListOf<String>()
+    val zeros = mutableListOf<String>()
+    for (row in numbers.indices) {
+      val num = numbers[row][columnIndex]
+      if (num == '1') {
+        ones.add(numbers[row])
+      } else {
+        zeros.add(numbers[row])
+      }
+    }
+
+    return if (ones.size < zeros.size) {
+      findCO2(ones, columnIndex + 1)
+    } else {
+      findCO2(zeros, columnIndex + 1)
+    }
+  }
 }
