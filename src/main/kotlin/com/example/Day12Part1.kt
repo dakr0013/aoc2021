18536d9dc5af9f2482687f3e293b2e98b8e13f9a
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-12 07:43:50 +0100

    day12
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- src/main/kotlin/com/example/Day12Part1.kt
+++ src/main/kotlin/com/example/Day12Part1.kt
@@ -1,5 +1,61 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(12)
+  val caveConnections = readLinesString(12)
+  val caves = mutableMapOf<String, Cave>()
+
+  for (connection in caveConnections) {
+    val (start, end) = connection.split("-")
+    val startCave = caves.getOrPut(start) { Cave(start, mutableSetOf()) }
+    val endCave = caves.getOrPut(end) { Cave(end, mutableSetOf()) }
+    startCave.connectedCaves.add(endCave)
+    endCave.connectedCaves.add(startCave)
+  }
+
+  val result = caves["start"]!!.countPathsTo("end")
+  println(result)
+}
+
+class Cave(val name: String, val connectedCaves: MutableSet<Cave>) {
+  private val isBig = name.uppercase() == name
+
+  private fun canVisit(currPath: String) =
+      if (isBig) {
+        true
+      } else {
+        !currPath.contains(name)
+      }
+
+  fun countPathsTo(target: String): Int {
+    return countPathsToRec(target, "start")
+  }
+
+  private fun countPathsToRec(target: String, currPath: String): Int {
+    return if (name == target) {
+      println("$currPath valid")
+      1
+    } else {
+      connectedCaves.sumOf { cave ->
+        if (cave.canVisit(currPath)) {
+          cave.countPathsToRec(target, "$currPath-${cave.name}")
+        } else {
+          println("$currPath-${cave.name} invalid")
+          0
+        }
+      }
+    }
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as Cave
+
+    if (name != other.name) return false
+
+    return true
+  }
+
+  override fun hashCode() = name.hashCode()
 }
--- src/main/kotlin/com/example/Day12Part2.kt
+++ src/main/kotlin/com/example/Day12Part2.kt
@@ -1,5 +1,66 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(12)
+  val caveConnections = readLinesString(12)
+  val caves = mutableMapOf<String, Cave2>()
+
+  for (connection in caveConnections) {
+    val (start, end) = connection.split("-")
+    val startCave = caves.getOrPut(start) { Cave2(start, mutableSetOf()) }
+    val endCave = caves.getOrPut(end) { Cave2(end, mutableSetOf()) }
+    startCave.connectedCaves.add(endCave)
+    endCave.connectedCaves.add(startCave)
+  }
+
+  val result = caves["start"]!!.countPathsTo("end")
+  println(result)
+}
+
+class Cave2(val name: String, val connectedCaves: MutableSet<Cave2>) {
+  private val isBig = name.uppercase() == name
+
+  private fun canVisit(currPath: String) =
+      if (isBig) {
+        true
+      } else if (name.matches("^(start|end)$".toRegex())) {
+        !currPath.contains(name)
+      } else {
+        val currentVisitedCaves = currPath.split("-")
+        val numVisitedSameCave =
+            currentVisitedCaves.groupBy { it }.filterKeys { it.lowercase() == it }.values
+        !currPath.contains(name) || numVisitedSameCave.all { it.size < 2 }
+      }
+
+  fun countPathsTo(target: String): Int {
+    return countPathsToRec(target, "start")
+  }
+
+  private fun countPathsToRec(target: String, currPath: String): Int {
+    return if (name == target) {
+      println("$currPath valid")
+      1
+    } else {
+      connectedCaves.sumOf { cave ->
+        if (cave.canVisit(currPath)) {
+          cave.countPathsToRec(target, "$currPath-${cave.name}")
+        } else {
+          println("$currPath-${cave.name} invalid")
+          0
+        }
+      }
+    }
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as Cave2
+
+    if (name != other.name) return false
+
+    return true
+  }
+
+  override fun hashCode() = name.hashCode()
 }
