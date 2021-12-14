9dca7482bc87fbb2d29109736467a0afcfc9764b
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-14 08:46:30 +0100

    day14
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- src/main/kotlin/com/example/Day14Part1.kt
+++ src/main/kotlin/com/example/Day14Part1.kt
@@ -1,5 +1,28 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(14)
+  val input = readLinesString(14)
+  val polymerTemplate = input.first()
+  val pairInsertionRules =
+      input.subList(2, input.size).associate {
+        val (pair, insertion) = it.split(" -> ")
+        pair to insertion
+      }
+
+  var polymer = polymerTemplate.toCharArray().toMutableList().map { it.toString() }
+  for (step in 1..10) {
+    val newPolymer = mutableListOf<String>()
+    for (i in polymer.dropLast(1).indices) {
+      val leftElement = polymer[i]
+      val rightElement = polymer[i + 1]
+      val pair = leftElement + rightElement
+      val insertion = pairInsertionRules[pair]!!
+      newPolymer.add(leftElement)
+      newPolymer.add(insertion)
+    }
+    newPolymer.add(polymer.last())
+    polymer = newPolymer
+  }
+  val elementCount = polymer.groupBy { it }.map { it.value.size }.sorted()
+  println(elementCount.last() - elementCount.first())
 }
--- src/main/kotlin/com/example/Day14Part2.kt
+++ src/main/kotlin/com/example/Day14Part2.kt
@@ -1,5 +1,43 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(14)
+  val input = readLinesString(14)
+  val polymerTemplate = input.first()
+  val pairInsertionRules =
+      input.subList(2, input.size).associate {
+        val (pair, insertion) = it.split(" -> ")
+        pair to insertion
+      }
+
+  val pairGeneratingRules =
+      pairInsertionRules.mapValues { listOf(it.key.first() + it.value, it.value + it.key.last()) }
+
+  val elementCounts =
+      pairInsertionRules
+          .values
+          .reduce { acc, s -> acc + s }
+          .toSet()
+          .associateWith { 0L }
+          .toMutableMap()
+  var pairCounts = pairInsertionRules.mapValues { 0L }.toMutableMap()
+  polymerTemplate.windowed(2, 1).forEach { pairCounts[it] = pairCounts[it]!!.inc() }
+  polymerTemplate.forEach { elementCounts[it] = elementCounts[it]!!.inc() }
+
+  for (step in 1..40) {
+    val newPairCounts = mutableMapOf<String, Long>()
+    for (pairCount in pairCounts) {
+      newPairCounts.merge(pairCount.key, 0) { old, new -> old + new }
+
+      val (generatedPair1, generatedPair2) = pairGeneratingRules[pairCount.key]!!
+      newPairCounts.merge(generatedPair1, pairCount.value) { old, new -> old + new }
+      newPairCounts.merge(generatedPair2, pairCount.value) { old, new -> old + new }
+
+      val insertedElement = pairInsertionRules[pairCount.key]!!.first()
+      elementCounts.merge(insertedElement, pairCount.value) { old, new -> old + new }
+    }
+    pairCounts = newPairCounts
+  }
+
+  val sortedCounts = elementCounts.values.sorted()
+  println(sortedCounts.last() - sortedCounts.first())
 }
