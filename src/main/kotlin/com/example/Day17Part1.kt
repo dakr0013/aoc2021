7fcff183c8747deeb9658d219f133825333c09f4
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-17 08:39:34 +0100

    day17
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- src/main/kotlin/com/example/Day17Part1.kt
+++ src/main/kotlin/com/example/Day17Part1.kt
@@ -1,5 +1,42 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(17)
+  val (xTargetRange, yTargetRange) =
+      readLinesString(17)
+          .first()
+          .replace("target area: x=", "")
+          .replace(" y=", "")
+          .split(",")
+          .map { range ->
+            val (start, end) = range.split("..").map { it.toInt() }
+            start..end
+          }
+
+  var highestPosition = -1
+  for (initialVelocityY in 0..1000) {
+    val currentHighestPosition = calcHighestPosition(initialVelocityY, yTargetRange)
+
+    if (currentHighestPosition > highestPosition) {
+      highestPosition = currentHighestPosition
+    }
+  }
+
+  println(highestPosition)
+}
+
+fun calcHighestPosition(initialVelocityY: Int, yTargetRange: IntRange): Int {
+  var currentY = 0
+  var currentVelocityY = initialVelocityY
+  var currentHighestPosition = currentY
+  while (currentY >= yTargetRange.first && currentY !in yTargetRange) {
+    currentY += currentVelocityY--
+    if (currentY > currentHighestPosition) {
+      currentHighestPosition = currentY
+    }
+  }
+  return if (currentY in yTargetRange) {
+    currentHighestPosition
+  } else {
+    -1
+  }
 }
--- src/main/kotlin/com/example/Day17Part2.kt
+++ src/main/kotlin/com/example/Day17Part2.kt
@@ -1,5 +1,45 @@
 package com.example
 
 fun main() {
-  val numbers = readFileInt(17)
+  val (xTargetRange, yTargetRange) =
+      readLinesString(17)
+          .first()
+          .replace("target area: x=", "")
+          .replace(" y=", "")
+          .split(",")
+          .map { range ->
+            val (start, end) = range.split("..").map { it.toInt() }
+            start..end
+          }
+
+  var count = 0
+  for (initialVelocityY in yTargetRange.first..1000) {
+    for (initialVelocityX in 0..xTargetRange.last) {
+      if (reachesTargetArea(initialVelocityX, initialVelocityY, xTargetRange, yTargetRange)) {
+        count++
+      }
+    }
+  }
+
+  println(count)
+}
+
+fun reachesTargetArea(
+    initialVelocityX: Int,
+    initialVelocityY: Int,
+    xTargetRange: IntRange,
+    yTargetRange: IntRange,
+): Boolean {
+  var currentX = 0
+  var currentY = 0
+  var currentVelocityX = initialVelocityX
+  var currentVelocityY = initialVelocityY
+  while (currentY >= yTargetRange.first &&
+      currentX <= xTargetRange.last &&
+      (currentX !in xTargetRange || currentY !in yTargetRange)) {
+    currentX += currentVelocityX
+    if (currentVelocityX > 0) currentVelocityX--
+    currentY += currentVelocityY--
+  }
+  return currentY in yTargetRange && currentX in xTargetRange
 }
