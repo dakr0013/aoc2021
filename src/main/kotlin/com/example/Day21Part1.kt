2f1944af93d0e8d31567fdd5915dda39c0f54f79
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-21 13:28:24 +0100

    day21
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- src/main/kotlin/com/example/Day21Part1.kt
+++ src/main/kotlin/com/example/Day21Part1.kt
@@ -1,5 +1,73 @@
 package com.example
 
+import kotlin.math.min
+
 fun main() {
-  val numbers = readFileInt(21)
+  val (player1StartingPos, player2StartingPos) =
+      readLinesString(21).map { it.split(" ").last().toInt() }
+
+  val dice = DeterministicDice(100)
+  val player1 = Player(player1StartingPos)
+  val player2 = Player(player2StartingPos)
+  val game = DiracDiceGame(dice, player1, player2)
+  game.playGame()
+
+  val losingPlayerScore = min(player1.totalScore, player2.totalScore)
+  println(dice.rollCount * losingPlayerScore)
+}
+
+class Player(initialPosition: Int) {
+  var totalScore = 0
+    private set
+  var position = initialPosition
+    private set
+
+  fun move(spaces: Int) {
+    position = ((position + spaces - 1) % 10) + 1
+    totalScore += position
+  }
+
+  fun hasWon() = totalScore >= 1000
+}
+
+class DiracDiceGame(val dice: DeterministicDice, val player1: Player, val player2: Player) {
+  private var isPlayer1Turn = true
+
+  fun playGame() {
+    while (!(player1.hasWon() || player2.hasWon())) {
+      nextTurn()
+    }
+  }
+
+  fun nextTurn() {
+    val result = dice.roll(3)
+    val currentPlayer =
+        if (isPlayer1Turn) {
+          player1
+        } else {
+          player2
+        }
+    currentPlayer.move(result)
+    isPlayer1Turn = !isPlayer1Turn
+  }
+}
+
+class DeterministicDice(val numSides: Int) {
+  var rollCount = 0
+    private set
+  var nextNumber = 1
+    private set
+
+  fun roll(): Int {
+    rollCount++
+    return nextNumber.also { nextNumber = (nextNumber % numSides) + 1 }
+  }
+
+  fun roll(times: Int): Int {
+    var sum = 0
+    for (i in 1..times) {
+      sum += roll()
+    }
+    return sum
+  }
 }
--- src/main/kotlin/com/example/Day21Part2.kt
+++ src/main/kotlin/com/example/Day21Part2.kt
@@ -1,5 +1,87 @@
 package com.example
 
+import kotlin.math.max
+
+var player1WinCount = 0UL
+var player2WinCount = 0UL
+val rollSumCount = calcRollSumCount()
+
 fun main() {
-  val numbers = readFileInt(21)
+  val (player1StartingPos, player2StartingPos) =
+      readLinesString(21).map { it.split(" ").last().toInt() }
+
+  playGame(player1StartingPos, player2StartingPos)
+  println(max(player1WinCount, player2WinCount))
+}
+
+fun calcRollSumCount(): Map<Int, ULong> {
+  val rollSumCount = mutableMapOf<Int, ULong>()
+  for (roll1 in 1..3) {
+    for (roll2 in 1..3) {
+      for (roll3 in 1..3) {
+        val sum = roll1 + roll2 + roll3
+        rollSumCount.merge(sum, 1UL) { old, new -> old + new }
+      }
+    }
+  }
+  return rollSumCount
+}
+
+data class PlayerState(val position: Int, val totalScore: Int = 0) {
+  fun hasWon() = totalScore >= 21
+
+  fun turn(sum: Int): PlayerState {
+    val newPosition = ((position + sum - 1) % 10) + 1
+    return PlayerState(newPosition, totalScore + newPosition)
+  }
+}
+
+data class GameState(
+    val isPlayer1Turn: Boolean,
+    val player1State: PlayerState,
+    val player2State: PlayerState,
+    val winMultiplier: ULong = 1UL,
+)
+
+fun playGame(player1InitPos: Int, player2InitPos: Int) {
+  val initialState =
+      GameState(
+          isPlayer1Turn = true,
+          player1State = PlayerState(player1InitPos),
+          player2State = PlayerState(player2InitPos),
+      )
+  continueGame(initialState)
+}
+
+fun continueGame(state: GameState) {
+  if (state.player1State.hasWon()) {
+    player1WinCount += state.winMultiplier
+    return
+  }
+  if (state.player2State.hasWon()) {
+    player2WinCount += state.winMultiplier
+    return
+  }
+
+  for ((rollOutcome, outcomeCount) in rollSumCount) {
+    if (state.isPlayer1Turn) {
+      val newPlayerState = state.player1State.turn(rollOutcome)
+      val newWinMultiplier = state.winMultiplier * outcomeCount
+      continueGame(
+          state.copy(
+              isPlayer1Turn = false,
+              player1State = newPlayerState,
+              winMultiplier = newWinMultiplier,
+          ))
+    } else {
+      val newPlayerState = state.player2State.turn(rollOutcome)
+      val newWinMultiplier = state.winMultiplier * outcomeCount
+      continueGame(
+          state.copy(
+              isPlayer1Turn = true,
+              player2State = newPlayerState,
+              winMultiplier = newWinMultiplier,
+          ))
+    }
+  }
 }
